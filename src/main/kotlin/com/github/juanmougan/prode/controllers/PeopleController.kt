922bc919688aec5b0d79d706848658f10d98ad7e
Juan Manuel Mougan <juanmougan@gmail.com>
2018-06-05 23:10:53 -0300

    Trying to fix POST

--- src/main/kotlin/com/github/juanmougan/prode/controllers/PeopleController.kt
+++ src/main/kotlin/com/github/juanmougan/prode/controllers/PeopleController.kt
@@ -2,13 +2,18 @@ package com.github.juanmougan.prode.controllers
 
 import com.github.juanmougan.prode.models.Person
 import com.github.juanmougan.prode.repositories.PeopleRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/people")
 class PeopleController(val peopleRepository: PeopleRepository) {
+
     @GetMapping("/")
     fun getAll() : List<Person> = peopleRepository.findAll()
+
+    @PostMapping("/")
+    fun create(@RequestBody person: Person): Person {
+        return peopleRepository.save(person)
+    }
+
 }
