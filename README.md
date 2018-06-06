c7b9683792f1fd391be97b8f5b1d66cd92b4d415
Juan Manuel Mougan <juanmougan@gmail.com>
2018-06-05 23:46:24 -0300

    Added missing methods and a README

---
+++ README.md
@@ -0,0 +1,30 @@
+### Some examples
+
+- To list all the people
+
+    curl -X POST 'localhost:8080/api/people' -d '{"name":"Juan Manuel"}' -H "Content-Type: application/json" | jq .
+
+- To create a new Person
+
+    curl 'localhost:8080/api/people' | jq .
+
+will return
+
+    [
+      {
+        "id": 1,
+        "name": "Juan Manuel"
+      }
+    ]
+
+- To get a Person by id
+
+    curl 'localhost:8080/api/people/1' | jq .
+
+- To edit some information
+
+    curl -X PUT 'localhost:8080/api/people/1' -d '{"name" : "Juan Manuel Mougan"}' -H "Content-Type: application/json" | jq .
+
+- Finally, to delete it
+
+    curl -X DELETE 'localhost:8080/api/people/1' | jq .
--- src/main/kotlin/com/github/juanmougan/prode/controllers/PeopleController.kt
+++ src/main/kotlin/com/github/juanmougan/prode/controllers/PeopleController.kt
@@ -2,6 +2,8 @@ package com.github.juanmougan.prode.controllers
 
 import com.github.juanmougan.prode.models.Person
 import com.github.juanmougan.prode.repositories.PeopleRepository
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
@@ -14,5 +16,32 @@ class PeopleController(val peopleRepository: PeopleRepository) {
 
     @PostMapping("/people")
     fun create(@Valid @RequestBody person: Person): Person = peopleRepository.save(person)
+
+    @GetMapping("/people/{id}")
+    fun getById(@PathVariable(value = "id") personId: Long): ResponseEntity<Person> {
+        return peopleRepository.findById(personId).map { person ->
+            ResponseEntity.ok(person)
+        }.orElse(ResponseEntity.notFound().build())
+    }
+
+    @PutMapping("/people/{id}")
+    fun updateById(@PathVariable(value = "id") personId: Long,
+                   @Valid @RequestBody newPerson: Person): ResponseEntity<Person> {
+        return peopleRepository.findById(personId).map { existingPerson ->
+            val updatedPerson: Person = existingPerson
+                    .copy(name = newPerson.name)
+            ResponseEntity.ok().body(peopleRepository.save(updatedPerson))
+        }.orElse(ResponseEntity.notFound().build())
+    }
+
+    @DeleteMapping("/people/{id}")
+    fun deleteById(@PathVariable(value = "id") personId: Long): ResponseEntity<Void> {
+
+        return peopleRepository.findById(personId).map { person  ->
+            peopleRepository.delete(person)
+            ResponseEntity<Void>(HttpStatus.OK)
+        }.orElse(ResponseEntity.notFound().build())
+
+    }
 
 }
