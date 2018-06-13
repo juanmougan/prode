16eae9d9fb94da4c03367a6c1db496e7e042caa8
Juan Manuel Mougan <juanmougan@gmail.com>
2018-06-13 12:50:19 -0300

    Added sample data to sh

--- createData.sh
+++ createData.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Create people
 curl -X POST 'localhost:8080/api/people' -d '{"name":"Juan Manuel"}' -H "Content-Type: application/json" | jq . ;
 curl -X POST 'localhost:8080/api/people' -d '{"name":"Pepito"}' -H "Content-Type: application/json" | jq . ;
@@ -7,3 +8,8 @@ curl -X POST 'localhost:8080/api/teams' -d '{"name":"Argentina"}' -H "Content-Ty
 curl -X POST 'localhost:8080/api/teams' -d '{"name":"Islandia"}' -H "Content-Type: application/json" | jq . ;
 curl -X POST 'localhost:8080/api/teams' -d '{"name":"Nigeria"}' -H "Content-Type: application/json" | jq . ;
 curl -X POST 'localhost:8080/api/teams' -d '{"name":"Croacia"}' -H "Content-Type: application/json" | jq . ;
+
+# Create Matches
+curl -X POST 'localhost:8080/api/matches' -d '{"homeTeam":{"id":3,"name":"Argentina"}, "awayTeam":{"id":4,"name":"Islandia"}, "round": "FIRST_ROUND"}' -H "Content-Type: application/json" | jq . ;
+curl -X POST 'localhost:8080/api/matches' -d '{"homeTeam":{"id":6,"name":"Croacia"}, "awayTeam":{"id":5,"name":"Nigeria"}, "round": "FIRST_ROUND"}' -H "Content-Type: application/json" | jq . ;
+curl -X POST 'localhost:8080/api/matches' -d '{"homeTeam":{"id":3,"name":"Argentina"}, "awayTeam":{"id":6,"name":"Croacia"}, "round": "FIRST_ROUND"}' -H "Content-Type: application/json" | jq . ;
